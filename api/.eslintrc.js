d11499e69f8d4516695024ab25bc27a86d8c1b8d
Kavin <kavin@Poonguzhalis-MacBook-Air.local>
2022-05-31 10:56:36 +0800

    Set up eslint

---
+++ api/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": "error"
+  },
+};
