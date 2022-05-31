0481088770fcd6ed1d8abdd1ddaeba97d6107e1d
Kavin <kavin@Poonguzhalis-MacBook-Air.local>
2022-05-31 08:08:37 +0800

    Add routes file

---
+++ api/src/controllers/users.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const formData = require('express-form-data')
+const routes = require('./src/routes')
+
+const app = express()
+app.use('*', cors())
+
+const PORT = process.env.PORT || 5000
+
+const http = require('http');
+
+const server = http.createServer(app);
+//const { Server } = require("socket.io");
+//const io = new Server(server);
+
+//const io = require("socket.io")(server)
+
+app.use(formData.parse({}))
+app.use(formData.format())
+app.use(formData.stream())
+app.use(formData.union())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const router = express.Router()
+app.use(router)
+
+server.listen(PORT, (err) => {
+    if (err) return console.log(`Cannot Listen on PORT: ${PORT}`)
+    return console.log(`Server is Listening on: http://localhost:${PORT}/`)
+})
+
+routes(router)
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(404).send("Sorry can't find that!")
+})
---
+++ api/src/routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+//router.post('/login', users)
+
+module.exports = router;
