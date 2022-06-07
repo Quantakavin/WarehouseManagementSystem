c410ede5ae24b8015dcc09d04eb83535c72e84b8
hyangwoosh <shine.thw@gmail.com>
2022-06-07 22:17:34 +0800

    updated rma apis

--- api/src/services/rma.service.js
+++ api/src/services/rma.service.js
@@ -1,61 +1,39 @@
 //database connection
 //more imports
 
-exports.getAllRMA = async () => {
-    try {
-      // const ORDER_BY = 'ORDER BY created_at ASC';
-      // const LIMIT = 'LIMIT 25';
-  
-      //const query = `SELECT * FROM "RMA" ${ORDER_BY} ${LIMIT}`;
-  
-      const result = await connection.query(query);
-      if (!result) return null;
-  
-      return result.rows;
-    } catch (e) {
-      console.log({ e });
-      return null;
-    }
-  };
+module.exports.getByRMANO = async (email) => {
+  //return knex.where('Email', email).select('UserID','Username','Password').from('User')
+  const query = `SELECT * FROM Rma WHERE RMANo = ?`;
+  return knex.raw(query, [email]);
+}
 
-  exports.create = async (data) => {
-    try {
-      const {  } = data;
-  
-      const created_at = new Date().toISOString();
-      const updated_at = new Date().toISOString();
-  
-      const values = {  };
-      const columns = Object.keys(values);
-      const rows = Object.values(values);
-      const queryColumns = columns.join(',');
-      const queryRows = serializer.SQLRows(rows).join(',');
-  
-      // Creating RMA
-      // stored procedure to create RMA
-  
-      const result = await connection.query(query);
-      if (!result) throw Error('RMA Not Created!');
-  
-    //   const id = result.rows[0]?.rma_id;
-  
-    //   return { id };
-    } catch (e) {
-      console.log({ e });
-      return null;
-    }
-  };
+module.exports.getAll = async () => {
+  const query = `SELECT * FROM Rma`;
+  return knex.raw(query);
+}
 
-  exports.getByRMANO = async (RMANO) => {
-    try {
-      //stored procedure to get RMA matching rmano
-  
-      const result = await connection.query(query);
-      if (!result) return null;
-  
-      return result.rows[0];
-    } catch (e) {
-      console.log({ e });
-      return null;
-    }
-  };
+module.exports.insert = async (DateTime, company, customer, contactperson, contactno, rmano, supplierrma, salesmanid, rmastatusid, instruction,) => {
+  return knex('Rma').insert({
+      CompanyID: company,
+      ContactPerson: contactperson,
+      ContactNo: contactno,
+      RMANo: rmano,
+      SupplierRMA: invoice,
+      SalesmanID: salesmanid,
+      SupplierRma: supplierrma,
+      RmaStatusID: rmastatusid,
+      Instruction: instruction
+  });
+}
+
+module.exports.update = async (rmastatusID, status) => {
+  return knex('RmaStatus').where('RmaStatusID', rmastatusID).update({
+      RmaStatus: status
+  });
+}
+
+module.exports.update = async (RMANo, instruction) => {
+  return knex('Rma').where('UserRMANo', RMANo).update({
+      Instruction: instruction
+  });
+}
