2d8b70847aebd537caf7273782bab21c9a5be5db
Hagen <57143868+icecreamrocket@users.noreply.github.com>
2022-06-16 20:42:30 +0800

    [Frontend] Changes to product.js

--- client/src/Pages/Products.js
+++ client/src/Pages/Products.js
@@ -1,40 +1,13 @@
-import React, { useState, useEffect } from "react";
-import SearchBar from "../Components/SearchBar";
-import CountryList from "../Components/countryList";
+import { useState } from "react";
+import "../Styles/styles.css";
 
-const SearchPage = (props) => {
-  const [input, setInput] = useState("");
-  const [countryListDefault, setCountryListDefault] = useState();
-  const [countryList, setCountryList] = useState();
-
-  const fetchData = async () => {
-    return await fetch("https://restcountries.eu/rest/v2/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryList(data);
-        setCountryListDefault(data);
-      });
-  };
-
-  const updateInput = async (input) => {
-    const filtered = countryListDefault.filter((country) => {
-      return country.name.toLowerCase().includes(input.toLowerCase());
-    });
-    setInput(input);
-    setCountryList(filtered);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+export default function Products() {
+  const [query, setQuery] = useState("");
 
   return (
-    <>
-      <h1>Country List</h1>
-      <SearchBar input={input} onChange={updateInput} />
-      <CountryList countryList={countryList} />
-    </>
+    <div className="Products">
+      <h1> Item Search </h1>
+      <input type="text" onChange={(e) => setQuery(e.target.value)} />
+    </div>
   );
-};
-
-export default SearchPage;
+}
