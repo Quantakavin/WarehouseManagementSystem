1cf893b8ae1fc529da33f3680bc013749ee214e9
Hagen <57143868+icecreamrocket@users.noreply.github.com>
2022-06-20 14:34:35 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/Quantakavin/FYP-Leaptron""
    This reverts commit 63fb2d6f16e175b596ad5c57f10e9c7d601d22c5.

--- client/src/Pages/Dashboards.tsx
+++ client/src/Pages/Dashboards.tsx
@@ -1,4 +1,4 @@
-import Topbar from "../components/header/TopBar";
+import TopBar from "../components/header/TopBar";
 import Sidebar from "../components/SideBar";
 import { Card } from "react-bootstrap";
 import "../styles/Dashboard.css";
@@ -96,9 +96,9 @@ function Dashboards() {
   return (
     <>
       <header>
-        <Topbar />
+        <TopBar />
       </header>
-      <div className="container">
+      <div className="dashboard-container">
         <Sidebar />
 
         <div className="dashboard">
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosPromise, AxiosResponse } from 'axios';
 import { Container } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
+import '../styles/form.scss';
 import TopBar from '../components/header/TopBar';
 import SubmitButton from '../components/form/SubmitButton';
 import ErrorAlert from '../components/form/ErrorAlert';
@@ -39,7 +40,7 @@ const Login = () => {
   })
 
   const onSubmit = (data: FormValues) => {
-    mutation.mutate(data, { onSuccess: () => navigate("/dashboard") })
+    mutation.mutate(data, { onSuccess: () => navigate("/userhome") })
   }
 
   return (
@@ -47,16 +48,18 @@ const Login = () => {
       <header>
         <TopBar />
       </header>
-      <div className="bluebackground">
+      <div style={{ backgroundColor: "#e3e8ee", height: "100vh", overflow: 'auto' }}>
+        <Container className="formlogo d-none d-sm-block">
+        </Container>
         <Container className="formcontainer shadow">
-          <h2 className="formheader">Login to your account</h2>
+          <h2 style={{ marginLeft: '8%', paddingBottom: 20, fontWeight: 700 }}>Login to your account</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div>
               <p className="formlabels">Email Address </p>
-              <div className="flexcontainer">
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <input className="formfield" type="email" {...register("email", { required: "Email cannot be empty", pattern: { value: /^\S+@\S+\.\S+$/i, message: "Please enter a valid email" } })} />
               </div>
-              <p className="errormsg">{errors.email?.message}</p>
+              <p style={{ color: "red", fontSize: "0.85em", marginLeft: "8%", marginTop: 5 }}>{errors.email?.message}</p>
             </div>
             <div>
               <p className="formlabels">Password </p>
@@ -66,7 +69,7 @@ const Login = () => {
                 <div style={{flexGrow: 1, paddingRight: 5}} className="flexcontainer" onClick={toggle}>{showPassword? <VisibilityIcon style={{ color: '#0A2540'}}  />:<VisibilityOffIcon style={{ color: '#0A2540'}}  />}</div>
                 </div>
               </div>
-              <p className="errormsg">{errors.password?.message}</p>
+              <p style={{ color: "red", fontSize: "0.85em", marginLeft: "8%", marginTop: 5 }}>{errors.password?.message}</p>
             </div>
             {mutation.isError && axios.isAxiosError(mutation.error) ? <ErrorAlert error={mutation.error} /> : <></>}
             <div className="flexcontainer" style={{ marginTop: 40 }}>
