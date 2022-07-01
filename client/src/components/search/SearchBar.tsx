ddfd680199208ddb66caeb183580ac30cdefb823
AnubisCylon <87166834+AnubisCylon@users.noreply.github.com>
2022-07-01 11:30:28 +0800

    some more tloan page formatting

--- client/src/components/search/SearchBar.tsx
+++ client/src/components/search/SearchBar.tsx
@@ -1,12 +1,12 @@
-import SearchBar from "material-ui-search-bar";
+// import SearchBar from "material-ui-search-bar";
 
-function ProductSearchBar() {
-  return (
-    <SearchBar
-    value={this.state.value}
-    onChange={(newValue) => this.setState({ value: newValue })}
-    onRequestSearch={() => (this.state.value)}
-  />
-  )
-}
-export default ProductSearchBar;
+// function ProductSearchBar() {
+//   return (
+//     <SearchBar
+//     value={this.state.value}
+//     onChange={(newValue) => this.setState({ value: newValue })}
+//     onRequestSearch={() => (this.state.value)}
+//   />
+//   )
+// }
+// export default ProductSearchBar;
--- client/src/components/tabs/tloantabs.tsx
+++ client/src/components/tabs/tloantabs.tsx
@@ -2,6 +2,13 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 export default function TLoanTabs() {
@@ -15,7 +22,8 @@ export default function TLoanTabs() {
           // get the data from the api
           const loan = await axios.get('https://localhost:5000/api/allLoan');
          
-          setLoan(loan.data);
+          setLoan(loan.data)
+          console.log(setLoan)
         }
       
         // call the function
@@ -46,9 +54,35 @@ export default function TLoanTabs() {
             </TabList>
 
             <TabPanel>
-                <div>
-                    
-                </div>
+            <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 1050 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Loan No.</TableCell>
+            <TableCell align="center">Start Date</TableCell>
+            <TableCell align="center">End Date</TableCell>
+            <TableCell align="center">Company Name</TableCell>
+            <TableCell align="center">Customer Email</TableCell>
+            <TableCell align="center">Actions</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow >
+              <TableCell component="th" scope="row" align="center" hidden>
+               
+              </TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              
+            </TableRow>
+        
+        </TableBody>
+      </Table>
+    </TableContainer>
 
             </TabPanel>
             <TabPanel>
