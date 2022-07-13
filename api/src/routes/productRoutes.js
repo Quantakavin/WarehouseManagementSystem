1093279c100ccaebcb2ca5a5599b6d90530124e9
LiauYanBin <87053939+LiauYanBin@users.noreply.github.com>
2022-07-13 22:13:47 +0800

    Get all products
    -get all products done

--- api/src/routes/productRoutes.js
+++ api/src/routes/productRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const productController = require('../controllers/productController');
 
-router.post('/products', productController.getAllProducts);
+router.get('/products', productController.getAllProducts);
 router.get('/products/:binProductPK', productController.getProductByPrimaryKey);
 router.post('/products/search', productController.searchFilterProducts);
 router.get('/testproducts', productController.getAllProductsTest);
--- api/src/services/productService.js
+++ api/src/services/productService.js
@@ -1,8 +1,8 @@
 const knex = require('../config/database');
 
-module.exports.getAll = async (offsetNo) => {
-    const query = `SELECT ItemName, BatchNo, Brand, Quantity FROM BinProduct LIMIT 100 OFFSET ?`;
-    return knex.raw(query, [parseInt(offsetNo)]);
+module.exports.getAll = async (limit, page) => {
+    const query = `SELECT BinProductPK, ItemName, BatchNo, Brand, Quantity, count(BinProductPK) OVER() AS full_count FROM BinProduct LIMIT ? OFFSET ?`;
+    return knex.raw(query, [Number(limit), Number(page)]);
 };
 
 module.exports.getByPrimaryKey = async (binProductPK) => {
