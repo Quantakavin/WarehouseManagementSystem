0ca57712389e1646d4fe316c8a4b3fb0b5221a45
AnubisCylon <87166834+AnubisCylon@users.noreply.github.com>
2022-07-21 16:44:50 +0800

    tloan extension completed

--- api/src/routes/tLoanRoutes.js
+++ api/src/routes/tLoanRoutes.js
@@ -13,11 +13,13 @@ router.get('/tloan/history', TLoanController.historyLoan);
 router.get('/tloan/pending', TLoanController.pendingLoan);
 router.put('/tloan/ready',TLoanController.readyLoan)
 router.put('/tloan/approve',TLoanController.approveLoan)
-router.put('/tloan/reject',TLoanController.rejectLoan)
+router.put('/tloan/reject/:TLoanNumber',TLoanController.rejectLoan)
 router.put('/tloan/due',TLoanController.dueLoan)
 router.put('/tloan/draft',TLoanController.draftLoan)
 router.put('/tloan/issued',TLoanController.issuedLoan)
 router.put('/tloan/picking',TLoanController.pickingLoan)
 router.get('/tloan/ManagerLoan', TLoanController.ManagerLoan);
+router.post('/tloan/extension',TLoanController.LoanExtend)
+router.get('/tloanid/:TLoanNumber', TLoanController.getIDofLoan)
 
 module.exports = router;
