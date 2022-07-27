24eabaa428ac94e8eedfaba64d48c2c528770625
Quantakavin <quantakavin@gmail.com>
2022-07-27 13:18:23 +0800

    tried out raycasting

---
+++ client/src/app/reducers/BinLocationSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../app/store";
+
+// Define a type for the slice state
+interface BinLocationState {
+  cameraposition: [x: number, y: number, z: number];
+}
+
+// Define the initial state using that type
+const initialState: BinLocationState = {
+    cameraposition: [0, 40, 150]
+};
+
+export const BinLocationSlice = createSlice({
+  name: "binlocation",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    ChangeLocation: (state, action: PayloadAction<{ cameraposition: [x: number, y: number, z: number] }>) => {
+      state.cameraposition = action.payload.cameraposition;
+    }
+  },
+});
+
+export const { ChangeLocation } = BinLocationSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCameraPosition = (state: RootState) => state.binlocation.cameraposition;
+
+export default BinLocationSlice.reducer;
--- client/src/pages/binlocations/BinLocations.tsx
+++ client/src/pages/binlocations/BinLocations.tsx
@@ -1,12 +1,18 @@
+import { Camera } from "@mui/icons-material";
 import { OrbitControls } from "@react-three/drei";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { Suspense } from "react";
-import { DoubleSide, TextureLoader } from "three";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { Suspense, useState } from "react";
+import * as THREE from "three";
 import Rack from "../../components/3Dmodels/Rack";
 import "../../styles/BinLocation.scss";
+// import { useAppSelector } from "../../app/hooks";
+// import { selectCameraPosition } from "../../app/reducers/BinLocationSlice";
+// import { useContextBridge } from "@react-three/drei"
+// import { useStore } from "react-redux";
+// import { store } from "../../app/store";
 
 const Floor = () => {
-  const colorMap = useLoader(TextureLoader, "Concrete030_4K_Color.png");
+  const colorMap = useLoader(THREE.TextureLoader, "Concrete030_4K_Color.png");
 
   return (
     // The mesh is at the origin
@@ -18,7 +24,7 @@ const Floor = () => {
     <mesh
       position={[0, -11.5, 0]}
       rotation={[Math.PI / 2, 0, 0]}
-      scale={[150, 150, 150]}
+      scale={[300, 300, 300]}
     >
       {/*
         The thing that gives the mesh its shape
@@ -29,7 +35,7 @@ const Floor = () => {
         The material gives a mesh its texture or look.
         In this case, it is just a uniform green
       */}
-      <meshBasicMaterial map={colorMap} side={DoubleSide} />
+      <meshBasicMaterial map={colorMap} side={THREE.DoubleSide} />
       {/*color="#7cb1d0*/}
     </mesh>
   );
@@ -56,43 +62,82 @@ const Floor = () => {
 //   );
 // };
 
+const Scene = () => {
+  // const [cameraPosition, setCameraPosition] = useState<[x: number, y: number, z: number]>([0, 40, 150])
+
+  const {
+    gl,
+    camera,
+    controls,
+  } = useThree();
+
+  const changeCameraPosition = (newPosition: [x: number, y: number, z: number]) => {
+    /*
+    camera.position.set(newPosition[0], newPosition[1], newPosition[2] + 10)
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    */alert(newPosition)
+    //camera.lookAt( new THREE.Vector3(newPosition[0], newPosition[1], newPosition[2]))
+    //alert(newPosition);
+  }
+
+  return (
+    <>
+     <color attach="background" args={["#c4e5d0"]} />
+     <ambientLight />
+     <pointLight position={[40, 40, 40]} />
+     <OrbitControls maxPolarAngle={Math.PI/2} maxDistance={150}/>
+     {/* <OrbitControls maxPolarAngle={Math.PI/2} maxDistance={150}/> */}
+     <Suspense fallback={null}>
+     {/* <ContextBridge> */}
+       <Rack position={[0, 0, 0]} areatag="A01" racktag="R01" changeposition={changeCameraPosition}/>
+       <Rack position={[20, 0, 0]} areatag="A01" racktag="R02" changeposition={changeCameraPosition}/>
+       <Rack position={[40, 0, 0]} areatag="A01" racktag="R03" changeposition={changeCameraPosition}/>
+       {/* <Rack position={[60, 0, 0]} areatag="A01" racktag="R04" changeposition={changeCameraPosition}/>
+       <Rack position={[60, 0, 18]} areatag="A01" racktag="R05" changeposition={changeCameraPosition}/>
+       <Rack position={[40, 0, 18]} areatag="A01" racktag="R06" changeposition={changeCameraPosition}/>
+       <Rack position={[20, 0, 18]} areatag="A01" racktag="R07" changeposition={changeCameraPosition}/>
+       <Rack position={[0, 0, -18]} areatag="A01" racktag="R08" changeposition={changeCameraPosition}/>
+       <Rack position={[20, 0, -18]} areatag="A01" racktag="R09" changeposition={changeCameraPosition}/>
+       <Rack position={[40, 0, -18]} areatag="A01" racktag="R10" changeposition={changeCameraPosition}/>
+       <Rack position={[60, 0, -18]} areatag="A01" racktag="R11" changeposition={changeCameraPosition}/>
+       <Rack position={[-20, 0, -18]} areatag="A01" racktag="R12" changeposition={changeCameraPosition}/> */}
+       <Floor />
+       {/* </ContextBridge> */}
+     </Suspense>
+
+     {/* <Suspense fallback={<p>Hi</p>}>
+ <primitive object={gltf.scene} />
+</Suspense> */}
+</>
+  )
+
+
+}
+
 const BinLocations = () => {
   // const gltf = useLoader(GLTFLoader, '/binsgrouped.glb')
+
+  // const ContextBridge = useContextBridge()
+
+  // useFrame(({ controls, camera }) => {
+  //   controls.target = cameraPosition;
+  //   controls.update()
+  // })
+
   return (
     <div className="binlocation">
       <h1 className="binlocationTitle">Warehouse Visualisation</h1>
       <div className="flexcontainer">
-        <Canvas
-          style={{
-            border: "solid 1px black",
-            height: 600,
-            marginTop: 20,
-            width: "80%",
-          }}
-        >
-          <color attach="background" args={["#c4e5d0"]} />
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <OrbitControls />
-          <Suspense fallback={null}>
-            <Rack position={[0, 0, 0]} areatag="A01" racktag="R01" />
-            <Rack position={[20, 0, 0]} areatag="A01" racktag="R02" />
-            <Rack position={[40, 0, 0]} areatag="A01" racktag="R03" />
-            <Rack position={[60, 0, 0]} areatag="A01" racktag="R04" />
-            <Rack position={[60, 0, 18]} areatag="A01" racktag="R05" />
-            <Rack position={[40, 0, 18]} areatag="A01" racktag="R06" />
-            <Rack position={[20, 0, 18]} areatag="A01" racktag="R07" />
-            <Rack position={[0, 0, -18]} areatag="A01" racktag="R08" />
-            <Rack position={[20, 0, -18]} areatag="A01" racktag="R09" />
-            <Rack position={[40, 0, -18]} areatag="A01" racktag="R10" />
-            <Rack position={[60, 0, -18]} areatag="A01" racktag="R11" />
-            <Rack position={[-20, 0, -18]} areatag="A01" racktag="R12" />
-            <Floor />
-          </Suspense>
-
-          {/* <Suspense fallback={<p>Hi</p>}>
-      <primitive object={gltf.scene} />
-    </Suspense> */}
+      <Canvas
+    camera={{ fov: 45, position: [0, 40, 150]}}
+     style={{
+       border: "solid 1px black",
+       height: 600,
+       marginTop: 20,
+       width: "80%",
+     }}
+   >
+        <Scene />
         </Canvas>
       </div>
     </div>
