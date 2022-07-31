1553dac6e3ab8cd93677156495bff9a504bdaa68
Hagen <57143868+icecreamrocket@users.noreply.github.com>
2022-07-31 22:22:42 +0800

    Added T-Loan Request Grouped by Companies (BarChart)

--- api/src/services/dashboardService.js
+++ api/src/services/dashboardService.js
@@ -111,12 +111,10 @@ module.exports.getRMACurrentStats = async () => {
     return knex.raw(query);
 };
 
-
-// Get TLoans Request Grouped By Type 
+// Get TLoans Request Grouped By Type
 module.exports.getTloanCompanies = async () => {
-    const query = `SELECT COUNT(TLoanID) AS Requests,  CompanyID as Company FROM TLoan  GROUP BY Company`;
+    const query = `SELECT CompanyID as Company, COUNT(TLoanID) AS Requests FROM TLoan  GROUP BY Company`;
     return knex.raw(query);
 };
-
 
 // Get RMA Request
--- client/src/Components/charts/BarCharts.tsx
+++ client/src/Components/charts/BarCharts.tsx
@@ -11,47 +11,50 @@ import {
   YAxis,
 } from "recharts";
 import config from "../../config/config";
+import tloan from "../../pages/tloans/tloan";
 import "../../styles/chart.scss";
 
-
-
 function Chart3({}) {
+  const [tloanrequest, settloanRequest] = useState([]);
 
-  const [tloanrequest, settloanRequest] = useState(null);
+  // Get Tloan Request Grouped By Companies
+
+  const getTloansCompanies = async () => {
+    const response = await axios.get(`${config.baseURL}/getTloanType`);
+    
+    settloanRequest(response.data);
+    console.log(tloanrequest);
+  };
+
+  useEffect(() => {
+      getTloansCompanies();
+  }, []);
 
 
+  // const data = [
+  //   {
+  //     time: "SERVO DYNAMICS PTE LTD",
+  //     users: 60,
+  //   },
+  //   {
+  //     time: "LEAPTRON ENGINEERING PTE LTD",
+  //     users: 45,
+  //   },
+  //   {
+  //     time: "DIRAK ASIA PTE LTD",
+  //     users: 15,
+  //   },
+  //   {
+  //     time: "PRECISION MOTION CONTROL PTE LTD",
+  //     users: 22,
+  //   },
+  //   {
+  //     time: "PORTWELL SINGAPORE PTE LTD",
+  //     users: 5,
+  //   },
+  // ];
 
-
-
-
-
-
-
-
-const data = [
-  {
-    time: "SERVO DYNAMICS PTE LTD",
-    users: 60,
-  },
-  {
-    time: "LEAPTRON ENGINEERING PTE LTD",
-    users: 45,
-  },
-  {
-    time: "DIRAK ASIA PTE LTD",
-    users: 15,
-  },
-  {
-    time: "PRECISION MOTION CONTROL PTE LTD",
-    users: 22,
-  },
-  {
-    time: "PORTWELL SINGAPORE PTE LTD",
-    users: 5,
-  },
-];
-
-// function Chart3() {
+  // function Chart3() {
   return (
     <Grid container>
       <Grid item xs={6}>
@@ -64,11 +67,11 @@ const data = [
             </Grid>
             <Grid item xs={12}>
               <ResponsiveContainer width="100%" aspect={4 / 1}>
-                <BarChart width={100} height={300} data={data}>
+                <BarChart width={100} height={300} data={tloanrequest}>
                   <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
-                  <XAxis dataKey="time" stroke="#8884d8" />
+                  <XAxis dataKey="Company" stroke="#8884d8" />
                   <YAxis />
-                  <Bar label={true} dataKey="users" fill="#8884d8" />
+                  <Bar label={true} dataKey="Requests" fill="#8884d8" />
                   <Tooltip />
                 </BarChart>
               </ResponsiveContainer>
@@ -86,7 +89,7 @@ const data = [
             </Grid>
             <Grid item xs={12}>
               <ResponsiveContainer width="100%" aspect={4 / 1}>
-                <BarChart width={100} height={300} data={data}>
+                <BarChart width={100} height={300} data={tloanrequest}>
                   <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
                   <XAxis dataKey="time" stroke="#8884d8" />
                   <YAxis />
