bb070e124445944f0b361b7adfef391509c61e6e
hyangwoosh <shine.thw@gmail.com>
2022-08-06 17:10:39 +0800

    sendgrid stuff

--- api/src/controllers/emailNotificationController.js
+++ api/src/controllers/emailNotificationController.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const dotenv = require('dotenv')
 const path = require('path')
 dotenv.config({path: path.resolve(__dirname, '../../.env')})
@@ -7,8 +8,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports.sendMail = ()=>{
     const msg = {
-        to, // Change to your recipient
-        from: 'bryanyang80@gmail.com', // Change to your verified sender
+        to: 'shine.thw@gmail.com', // Change to your recipient
+        from: 'shine.thw@gmail.com', // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'noobs',
         html: '<strong>fk</strong>',
