9696269cbe1adc67fe821de9b3e0b81e8780a512
Quantakavin <quantakavin@gmail.com>
2022-08-07 14:56:17 +0800

    add redis

--- api/src/config/caching.js
+++ api/src/config/caching.js
@@ -1,6 +1,13 @@
 const redis = require('redis');
 //import { createClient } from 'redis';
-const redisClient = redis.createClient();
+let redisClient = null;
+
+if(process.env.REDIS_URL){
+    let redisURL = url.parse(process.env.REDIS_URL);
+    redisClient = redis.createClient(redisURL)
+} else {
+    redisClient = redis.createClient()
+}
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
