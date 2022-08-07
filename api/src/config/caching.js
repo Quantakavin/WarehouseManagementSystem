9b5e853448ae414e1d00735d4f7b5ccd122bedc0
Quantakavin <quantakavin@gmail.com>
2022-08-07 15:07:29 +0800

    add console log)

--- api/src/config/caching.js
+++ api/src/config/caching.js
@@ -2,14 +2,13 @@ const redis = require('redis');
 //import { createClient } from 'redis';
 let redisClient = null;
 
-console.log("redisurl is ", process.env.REDIS_URL)
 if(process.env.REDIS_URL){
     let redisURL = process.env.REDIS_URL;
     redisClient = redis.createClient(redisURL)
 } else {
     redisClient = redis.createClient()
 }
-
+console.log("redisurl is ", process.env.REDIS_URL)
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
 redisClient.connect();
