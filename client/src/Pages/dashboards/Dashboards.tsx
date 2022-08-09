03dd085127b4e3b6e9c54df3970741cc8f9c70f6
hyangwoosh <shine.thw@gmail.com>
2022-08-09 18:11:04 +0800

    View Products buttons fixed

--- client/src/Pages/dashboards/Dashboards.tsx
+++ client/src/Pages/dashboards/Dashboards.tsx
@@ -1,4 +1,4 @@
-import Cards from "../../Components/cards/Cards";
+import Cards from "../../Components/Cards/cards";
 import BarAndPieCharts from "../../Components/charts/BarAndPieCharts";
 import LineCharts from "../../Components/charts/LineCharts";
 import "../../Styles/Dashboard.scss";
