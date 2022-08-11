d3961d1d31f8f01fca8b404b7617990116b8ee14
Quantakavin <quantakavin@gmail.com>
2022-08-11 18:49:01 +0800

    disable knex debug

--- api/src/db/knexfile.js
+++ api/src/db/knexfile.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 module.exports = {
     development: {
         client: 'mysql',
-        debug: true,
+        // debug: true,
         connection: {
             host: process.env.DBHOST,
             port: 3307,
