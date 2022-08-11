e66a914bc786a7b0977fe444564ecb715bb89689
Quantakavin <quantakavin@gmail.com>
2022-08-12 07:18:41 +0800

    spelling error

--- api/src/controllers/notificationGroupController.js
+++ api/src/controllers/notificationGroupController.js
@@ -141,7 +141,7 @@ module.exports.updateNotificationGroup = async (req, res) => {
                 company,
                 notifications
             );
-            redisClient.del(`notificationGroups${notificationGroupID}`);
+            redisClient.del(`notificationGroup${notificationGroupID}`);
             return res.status(200).json({ message: 'Notification Group updated successfully!' });
         }
         return res.status(404).json({ message: 'Cannot find Notification Group with that id' });
