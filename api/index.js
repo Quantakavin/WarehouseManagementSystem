799d63f3c90d376ba4309b516dd4f2680e96202e
Quantakavin <quantakavin@gmail.com>
2022-08-16 12:37:36 +0800

    remove console logs

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ client.on('connect', function () {
 
     client.on('message', (topic, message) => {
         if (topic === 'quantity') {
-            console.log("quantity message received")
+            // console.log("quantity message received")
             const parsedmessage = JSON.parse('[' + message + ']');
             productController.updateQuantity(
                 parsedmessage[0].ItemNo,
